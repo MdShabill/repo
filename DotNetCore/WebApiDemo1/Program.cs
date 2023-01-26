c640ef1777219cf23d313bbc7b33d6e4cb2bd511
MdShabill <122606521+MdShabill@users.noreply.github.com>
2023-01-26 20:35:28 +0530

    WebApiDemo1 Program.cs

--- DotNetCore/WebApiDemo1/Program.cs
+++ DotNetCore/WebApiDemo1/Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using WebApiDemo1.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+// Get the configuration 
+IConfigurationRoot? configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+
+builder.Services.AddTransient<ICustomerRepository>((svc) =>
+{
+    string sqlConnectionString = configuration.GetConnectionString("CustomerDBConnection");
+    return new CustomerRepository(sqlConnectionString);
+});
 
 // Add services to the container.
 
