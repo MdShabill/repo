141ded64d16f250e126527b068d679343a59058a
MdShabill <122606521+MdShabill@users.noreply.github.com>
2023-04-28 23:54:53 +0530

    Created function and variable in console application

---
+++ DotNetCore/ConsoleApplication/VariableDemo.cs/Customer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableDemo
+{
+    public class Customer
+    {
+        //string firstName = "Md";
+        //string middleName = "Shabill";
+        //string lastName = "Irfani";
+
+        public void GetFullName()
+        {
+            string firstName = "Md";
+            string middleName = "Shabill";
+            string lastName = "Irfani";
+            string fullName = firstName + " " + middleName + " " + lastName;
+
+            // - Concatenation
+            Console.WriteLine("My Name Is " + fullName + " Thank You");
+            Console.WriteLine("My Name is  " + firstName + " " + middleName + " " + lastName + " Thank you");
+
+            //dynamic String using $
+            Console.WriteLine($"My Name Is {firstName} {middleName} {lastName} Thank You");
+        }
+
+
+        public void GetFullName_Approach2(string firstName, string middleName, string lastName)
+        { 
+            Console.WriteLine($"My Name Is {firstName} {middleName} {lastName} Thank You");
+        }
+
+        //Fnction - GetNameAndAge
+        //Input --- fullName, Age
+        //String display - "Hey my full name is ..... and my age is ...."
+
+        public void GetNameAndAge(string fullName, int age)
+        {
+            Console.WriteLine($"Hey My Full Name Is {fullName} And My Age Is {age}");
+        }
+
+        public void GetMoviesDetail(string searchKeyWord, string sortColumnName, string sortOrder)
+        {
+            string sqlBasicQuery = "Select * From Movies ";
+
+            if (!string.IsNullOrWhiteSpace(searchKeyWord))
+                sqlBasicQuery += "Where ActorName LIKE '%' + @search + '%' Or ActressName LIKE '%' + @search + '%' Or Title Like '%' + @search + '%' ";
+
+            sqlBasicQuery += $"Order By {sortColumnName} {sortOrder}";
+
+            Console.WriteLine(sqlBasicQuery);
+        }
+    }
+}
---
+++ DotNetCore/ConsoleApplication/VariableDemo.cs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace VariableDemo
+{
+    public class Program
+    {
+        //string firstName = "Md";
+        //string middleName = "Shabill";
+        //string lastName = "Irfani";
+
+        //string fruitsBox = "Mango";
+        //string SnacksBox = "Chocolate";
+        //string dessertsBox = "Baked Cham cham";
+        //string clothesBox = "Shirt";
+
+        static void Main(string[] args)
+        {
+            Customer customer = new();
+            customer.GetFullName();
+
+
+            // supplying hard code value
+            //customer.GetFullName_Approach2("Md", "Shabill", "Irfani");
+            //customer.GetFullName_Approach2("Md", "Shahrukh", "Irfani");
+            //customer.GetFullName_Approach2("Md", "Zahid", "Ahmad");
+
+            //Console.WriteLine("---------------------------\n");
+
+            //Console.WriteLine("Enter First Name");
+            //string fName = Console.ReadLine();
+
+            //Console.WriteLine("Enter Middle Name");
+            //string mName = Console.ReadLine();
+
+            //Console.WriteLine("Enter Last Name");
+            //string lName = Console.ReadLine();
+
+            //customer.GetFullName_Approach2(fName, mName, lName);
+
+            //Console.WriteLine("----------------------------\n");
+
+            //Console.WriteLine("Enter your Full Name");
+            //string fullName = Console.ReadLine();
+
+            //Console.WriteLine("Enter your Age");
+            //int age = int.Parse(Console.ReadLine());
+
+            //customer.GetNameAndAge(fullName, age);
+
+
+            //string firstName = "Md";
+            //string middleName = "Shabill";
+            //string lastName = "Irfani";
+            //string fullName = firstName + " " + middleName+ " " + lastName;
+
+            //Console.WriteLine("My Name Is "+ fullName + " Thank You");
+
+            //Console.WriteLine("My Name is  " + firstName + " " + middleName + " " + lastName + " Thank you");
+
+            //Console.WriteLine($"My Name Is {firstName} {middleName} {lastName} Thank You");
+
+            customer.GetMoviesDetail("SRK", "ActorName", "DESC");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
