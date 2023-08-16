8c8571296d2ac74b9efd4dbd7b162faceb496aa9
MdShabill <122606521+MdShabill@users.noreply.github.com>
2023-08-17 00:44:39 +0530

    Added a quantity text box in product view and add submit button buy now and in data base created a new orders table

--- DotNetCore/DataBase/EcommerceDB/dbo.Orders.sql
+++ DotNetCore/DataBase/EcommerceDB/dbo.Orders.sql
@@ -1,9 +1,14 @@
-CREATE TABLE Orders
+Create Table Orders
 (
-	Id INT IDENTITY(1,1),
-	CustomerId INT,
-	OrderDate DATETIME,
-	Amount INT,
-	ProductName Int,
-	PRIMARY KEY (Id)
+	Id int Identity(1,1),
+	CustomerId int Not Null,
+	ProductId int Not Null,
+	OrderDate DateTime Not Null,
+	ShippingAddress Nvarchar(200) Not Null,
+	ExpectedDeliveryDate DateTime Not Null,	
+	TotalAmount Decimal(10,3) Null,
+	Quantity int Not Null,	
+	Primary Key (Id),
+	FOREIGN KEY (CustomerId) REFERENCES Customers (Id),
+	FOREIGN KEY (ProductId) REFERENCES Products (Id)
 )
