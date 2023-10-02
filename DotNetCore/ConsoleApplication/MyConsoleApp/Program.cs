e0b025558cc21a5398fc06e1a98bf01ff0ff1b9f
MdShabill <122606521+MdShabill@users.noreply.github.com>
2023-10-02 20:19:43 +0530

    hello Check the number in array

---
+++ DotNetCore/ConsoleApplication/MyConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+//---- Check whether that number exists in array or not -----
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.Write("Enter Number: ");
+
+int inputNumber = int.Parse(Console.ReadLine());
+
+//bool exists = false;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] == inputNumber)
+    {
+        //exists = true;
+        Console.WriteLine($"{inputNumber} exist in the array.");
+        return;
+    }
+}
+
+Console.WriteLine($"{inputNumber} does not exist in the array.");
+
