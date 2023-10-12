c05e15d1b541b59e8e3fc418f727cc1c0dd1c07c
MdShabill <122606521+MdShabill@users.noreply.github.com>
2023-10-12 12:23:36 +0530

    I have applied validation to the property in action

--- DotNetCore/MVC/ShopEase/Controllers/ProductController.cs
+++ DotNetCore/MVC/ShopEase/Controllers/ProductController.cs
@@ -5,24 +5,25 @@ using AutoMapper;
 using ShopEase.DataModels.Product;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ShopEase.Repositories;
+using System.Reflection;
 
 namespace ShopEase.Controllers
 {
     public class ProductController : Controller
     {
         IProductRepository _productRepository;
-        IProductBrandRepository _prductBrandRepository;
+        IProductBrandRepository _productBrandRepository;
         IProductCategoryReopsitory _productCategoryReopsitory;
         IProductSupplierRepository _productSupplierRepository;
         IMapper _imapper;
 
         public ProductController(IProductRepository productRepository, 
-                                 IProductBrandRepository prductBrandRepository,
+                                 IProductBrandRepository productBrandRepository,
                                  IProductCategoryReopsitory productCategoryReopsitory,
                                  IProductSupplierRepository productSupplierRepository)
         {
             _productRepository = productRepository;
-            _prductBrandRepository = prductBrandRepository;
+            _productBrandRepository = productBrandRepository;
             _productCategoryReopsitory = productCategoryReopsitory;
             _productSupplierRepository = productSupplierRepository;
 
@@ -47,7 +48,69 @@ namespace ShopEase.Controllers
 
         public IActionResult Add()
         {
-            List<ProductBrand> productBrands = _prductBrandRepository.GetBrands();
+            DropDownSelectList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductAddVm productAddVm) 
+        {
+            DropDownSelectList();
+
+            if (string.IsNullOrWhiteSpace(productAddVm.ProductName))
+            {
+                ViewBag.ErrorMessage = "Product Name should not be blank ";
+                return View();
+            }
+
+            if (productAddVm.ProductName.Length > 20)
+            {
+                ViewBag.ErrorMessage = "Product Name should be 20 characters or less";
+                return View();
+            }
+
+            if (productAddVm.BrandId == 0)
+            {
+                ViewBag.ErrorMessage = "Please Select a Valid Brand Id Option ";
+                return View();
+            }
+
+            if (productAddVm.Price > 300000)
+            {
+                ViewBag.ErrorMessage = "Product price should be 300000 or less ";
+                return View();
+            }
+
+            if (productAddVm.Discount > 60)
+            {
+                ViewBag.ErrorMessage = "Product discount should be 60% or less ";
+                return View();
+            }
+
+            if (productAddVm.CategoryId == 0)
+            {
+                ViewBag.ErrorMessage = "Please Select a Valid Category Id Option ";
+                return View();
+            }
+
+            if (productAddVm.SupplierId == 0)
+            {
+                ViewBag.ErrorMessage = "Please Select a Valid Supplier Id Option ";
+                return View();
+            }
+
+            ProductAdd productAdd = _imapper.Map<ProductAddVm, ProductAdd>(productAddVm);
+            int affrectedRowCount = _productRepository.Add(productAdd);
+            if (affrectedRowCount > 0)
+            {
+                TempData["SuccessMessageForAdd"] = "Product Add Successful ";
+            }
+            return RedirectToAction("Index", productAddVm);
+        }
+
+        private void DropDownSelectList()
+        {
+            List<ProductBrand> productBrands = _productBrandRepository.GetBrands();
             ViewBag.Brands = new SelectList(productBrands, "Id", "BrandName");
 
             List<ProductCategory> categories = _productCategoryReopsitory.GetCategories();
@@ -55,25 +118,11 @@ namespace ShopEase.Controllers
 
             List<ProductSupplier> productSuppliers = _productSupplierRepository.GetSuppliers();
             ViewBag.Suppliers = new SelectList(productSuppliers, "Id", "SupplierName");
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Add(ProductAddVm productAddVm) 
-        {
-            ProductAdd productAdd = _imapper.Map<ProductAddVm, ProductAdd>(productAddVm);
-            int affrectedRowCount = _productRepository.Add(productAdd);
-            if (affrectedRowCount > 0) 
-            {
-                TempData["SuccessMessageForAdd"] = "Product Add Successful ";
-            }
-            return RedirectToAction("Index");
         }
 
         private List<ProductBrandVm> GetBrands() 
         {
-            List<ProductBrand> productBrands = _prductBrandRepository.GetBrands();
+            List<ProductBrand> productBrands = _productBrandRepository.GetBrands();
             List<ProductBrandVm> productBrandsVm = _imapper.Map<List<ProductBrand>, List<ProductBrandVm>>(productBrands);
             return productBrandsVm;
         }
