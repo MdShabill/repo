bf1c1abcc8f634f1b6b06b3b657840bc65981765
MdShabill <mdshabill8@gmail.com>
2024-03-07 23:31:41 +0530

    Added a new query where we fetched 2nd highest salary from employee table by using CTE

--- Database/Query/dbo.Fetch Highest Salary.sql
+++ Database/Query/dbo.Fetch Highest Salary.sql
@@ -1,6 +1,19 @@
---Q:2 Find the 2nd highest salary from teachers table?
+--Approach: 1
+--Q:2 Find the employee details which employee have 2nd highest salary?
 
 SELECT TOP 1 * FROM
 	(SELECT TOP 2 * FROM Employees
 	ORDER BY Salary DESC) AS SecondHighestSalary
+ORDER BY Salary ASC
+
+--Approach: 2
+-- Find the 2nd highest salary from the employee table by Using CTE instead of Subquery?
+
+WITH SecondHighestSalary AS (
+    SELECT TOP 2 * 
+    FROM Employees
+    ORDER BY Salary DESC
+)
+SELECT TOP 1 *
+FROM SecondHighestSalary
 ORDER BY Salary ASC
