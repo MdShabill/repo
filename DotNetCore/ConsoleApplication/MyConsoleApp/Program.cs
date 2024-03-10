182058d663f076f99c2507e596b35469687cf4a0
MdShabill <mdshabill8@gmail.com>
2024-03-10 20:08:48 +0530

    Added a new class Check Exists Number and call in Program.cs

--- DotNetCore/ConsoleApplication/MyConsoleApp/Program.cs
+++ DotNetCore/ConsoleApplication/MyConsoleApp/Program.cs
@@ -1,28 +1,17 @@
 ﻿using System;
 
-//Console.WriteLine("Hello, World!");
-//
-////---- Check whether that number exists in array or not -----
-//
-//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//
-//Console.Write("Enter Number: ");
-//
-//int inputNumber = int.Parse(Console.ReadLine());
-//
-////bool exists = false;
-//
-//for (int i = 0; i < numbers.Length; i++)
-//{
-//    if (numbers[i] == inputNumber)
-//    {
-//        //exists = true;
-//        Console.WriteLine($"{inputNumber} exist in the array.");
-//        return;
-//    }
-//}
-//
-//Console.WriteLine($"{inputNumber} does not exist in the array.");
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //CheckExistsNumber
+            CheckExistsNumber existsNumber = new CheckExistsNumber();
+            existsNumber.CheckNumber();
+        }
+    }
+}
 
 //---Array - Print the first occurrence index of that number----   
 
@@ -233,20 +222,20 @@
 
 //Write a pattern  program using loop
 
-Console.Write("Enter the number of rows for the pattern: ");
-int numRows = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Enter the number of rows for the pattern: ");
+//int numRows = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Pattern:");
+//Console.WriteLine("Pattern:");
 
-for (int i = 1; i <= numRows; i++)
-{
-    for (int j = 1; j <= i; j++)
-    {
-        Console.Write("* ");
-    }
-    Console.WriteLine();
-}
-Console.ReadLine();
+//for (int i = 1; i <= numRows; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write("* ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.ReadLine();
 
 
 
