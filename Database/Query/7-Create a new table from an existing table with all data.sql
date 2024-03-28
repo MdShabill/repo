97cbae91f40819b45dc6a171c2b310795c6418c6
MdShabill <mdshabill8@gmail.com>
2024-03-29 01:18:20 +0530

    Created a new table from an existing table with data and without data in BookingDB

---
+++ Database/Query/7-Create a new table from an existing table with all data.sql
@@ -0,0 +1,7 @@
+--To create a new table from an existing table with including all the data
+
+Select * From Bookings
+
+Select * Into NewBookings From Bookings
+
+Select * From NewBookings
---
+++ Database/Query/8- Create a new table from an existing table without any data.sql
@@ -0,0 +1,8 @@
+--To create a new table from an existing table without any actual data
+
+Select * From Passengers
+
+Select * Into NewPassengers From Passengers
+Where 1 = 0
+
+Select * From NewPassengers
