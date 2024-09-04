40ccb8e245b363ad4ffeb0dc446e3cb89b3e3c47
MdShabill <mdshabill8@gmail.com>
2024-09-04 19:27:07 +0530

    Changed result format on UI in Index.cshtml the calculation and format in DailyAttendanceVm

--- DotNetCore/MVC/CostConstruct/ConstructionApplication/ViewModels/DailyAttendanceVm/DailyAttendanceVm.cs
+++ DotNetCore/MVC/CostConstruct/ConstructionApplication/ViewModels/DailyAttendanceVm/DailyAttendanceVm.cs
@@ -8,5 +8,14 @@
         public decimal MasterMasonAmount { get; set; }
         public decimal LabourAmount { get; set; }
         public decimal TotalAmount { get; set; }
+
+        public string TotalCount
+        {
+            get
+            {
+                int totalCount = TotalMasterMason + TotalLabour;
+                return $"{totalCount} ({TotalMasterMason} + {TotalLabour})";
+            }
+        }
     }
 }
