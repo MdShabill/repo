37bee3b36e7ff37b172347c2929a49ce4b3aac6c
MdShabill <mdshabill8@gmail.com>
2025-01-26 23:58:14 +0530

    Added New Repository class for CRUD and in sql query we user stored procedure

---
+++ DotNetCore/MVC/CostConstruct/ConstructionApplication.Repository/ContractorCRUD.cs
@@ -0,0 +1,65 @@
+﻿using ConstructionApplication.Core.DataModels.Contractor;
+using ConstructionApplication.Core.Enums;
+using ConstructionApplication.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionApplication.Repositories
+{
+    public class ContractorCRUD : IContractorCRUD
+    {
+        private readonly string _connectionString;
+
+        public ContractorCRUD(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Contractor> GetAll(int? jobCategoryId, int? id)
+        {
+            using (SqlConnection sqlConnection = new(_connectionString))
+            {
+                string sqlQuery = "EXEC GetContractors";
+
+                SqlDataAdapter sqlDataAdapter = new(sqlQuery, sqlConnection);
+                sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@jobCategoryId", jobCategoryId ?? (object)DBNull.Value);
+                sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@id", id ?? (object)DBNull.Value);
+                DataTable dataTable = new();
+                sqlDataAdapter.Fill(dataTable);
+
+                List<Contractor> contractors = new();
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    Contractor contractor = new()
+                    {
+
+
+                        JobCategoryId = (int)dataTable.Rows[i]["JobCategoryId"],
+                        JobTypes = (string)dataTable.Rows[i]["JobTypes"],
+                        ContractorId = (int)dataTable.Rows[i]["ContractorId"],
+                        ContractorName = (string)dataTable.Rows[i]["ContractorName"],
+                        Gender = (GenderTypes)dataTable.Rows[i]["Gender"],
+                        DOB = (DateTime)dataTable.Rows[i]["DOB"],
+                        MobileNumber = (string)dataTable.Rows[i]["MobileNumber"],
+                        ReferredBy = dataTable.Rows[i]["ReferredBy"] != DBNull.Value ? (string)dataTable.Rows[i]["ReferredBy"] : null,
+
+                        AddressLine1 = dataTable.Rows[i]["AddressLine1"] != DBNull.Value ? (string)dataTable.Rows[i]["AddressLine1"] : null,
+                        AddressTypeId = dataTable.Rows[i]["AddressTypeId"] != DBNull.Value ? (int)dataTable.Rows[i]["AddressTypeId"] : 0,
+                        AddressTypes = dataTable.Rows[i]["AddressTypes"] != DBNull.Value ? (string)dataTable.Rows[i]["AddressTypes"] : null,
+                        CountryId = dataTable.Rows[i]["CountryId"] != DBNull.Value ? (int)dataTable.Rows[i]["CountryId"] : 0,
+                        CountryName = dataTable.Rows[i]["CountryName"] != DBNull.Value ? (string)dataTable.Rows[i]["CountryName"] : null,
+                        PinCode = dataTable.Rows[i]["PinCode"] != DBNull.Value ? (int)dataTable.Rows[i]["PinCode"] : 0
+                    };
+                    contractors.Add(contractor);
+                }
+                return contractors;
+            }
+        }
+    }
+}
---
+++ DotNetCore/MVC/CostConstruct/ConstructionApplication.Repository/Interfaces/IContractorCRUD.cs
@@ -0,0 +1,14 @@
+﻿using ConstructionApplication.Core.DataModels.Contractor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionApplication.Repository.Interfaces
+{
+    public interface IContractorCRUD
+    {
+        public List<Contractor> GetAll(int? jobCategoryId, int? id);
+    }
+}
